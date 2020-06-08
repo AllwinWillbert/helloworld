1dbc1c1af8d7962260f59be369b814de0c51ea58
AllwinWillbert <66547141+AllwinWillbert@users.noreply.github.com>
2020-06-08 10:51:11 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,3 +1,6 @@
 print('andavaruke magimai undaguga')
 print('christhuvin mulamai amathiyum nilaivalvum urithaguga')
 print('parisuthavai en aanmavil irangi ummodu inainthu vaala kirubai tharum aandavare')
+
+
+print('amen')
