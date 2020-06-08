b2d43e5e06908a04f9cc61e5e39d488271588cd7
AllwinWillbert <allwinwillbert1997@gmail.com>
2020-06-08 10:25:50 +0530

    parisuthavi is added

--- first.py
+++ first.py
@@ -1,2 +1,3 @@
 print('andavaruke magimai undaguga')
 print('christhuvin mulamai amathiyum nilaivalvum urithaguga')
+print('parisuthavai en aanmavil irangi ummodu inainthu vaala kirubai tharum aandavare')
